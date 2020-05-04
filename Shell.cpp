2fbd065b4e349aa5c02c2d0ac64ea29bf2700b33
Tashik Moin <k180142@nu.edu.pk>
2020-05-04 01:36:27 -0700

    Added Bug Free Tokenizer

--- Shell.cpp
+++ Shell.cpp
@@ -15,13 +15,53 @@ class Shell
 
     Shell()
     {
-        User_Input = "" ;
+        this->User_Input = "" ;
+        this->Argument_List = NULL ;
+    }
+
+    void Load_Shell()
+    {
         while(1)
         {
             cout << "\033[1;31m >>> \033[0m";
             getline( cin , this->User_Input ) ;
+            Tokenize() ;
             system(this->User_Input.c_str()) ;
         }
+    }
+
+    int Total_Words( )
+    {
+        int i = 0 , Count = 1 ;
+        while( i < this->User_Input.length() )
+        {
+            if(this->User_Input[i] == ' ')
+            {
+                Count++ ;
+            }
+            i++ ;
+        }
+        return Count ;
+    }
+
+    void Tokenize()
+    {
+        int x = Total_Words() ;
+        this->Argument_List = new string[ x * 20 ] ;
+        for( int i = 0 , j = 0 ; i < this->User_Input.length() ; ++i )
+        {
+            if( this->User_Input[i] == ' ' )
+            {
+                ++i ; ++j ;
+            }
+            this->Argument_List[j] += this->User_Input[i] ;
+        }
+
+        for( int i = 0 ; i < this->Total_Words() ; ++i )
+        {
+            cout<<this->Argument_List[i]<<endl ;
+        }
+        cin.get() ;
     }
 } ;
 
@@ -30,4 +70,5 @@ class Shell
 int main() 
 {
     Shell *My_Shell = new Shell() ;
+    My_Shell->Load_Shell() ;
 }
